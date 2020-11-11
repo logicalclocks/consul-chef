8d764c1ad9245afaf97961b4d2e18a0ae5857576
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-11-11 18:50:36 +0100

    Release version: 2.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures HashiCorp Consul'
 long_description 'Installs/Configures HashiCorp Consul for Hopsworks'
 source_url 'https://github.com/logicalclocks/consul-chef'
 issues_url 'https://github.com/logicalclocks/consul-chef/issues'
-version '2.0.0'
+version '2.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'conda'
