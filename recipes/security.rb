bee71f418c716bb4c555a792016645471a2a4cbb
Antonios Kouzoupis <antonios@logicalclocks.com>
2020-12-22 10:05:12 +0100

    [HOPSWORKS-2241] Get Hopsworks private IP only if needed

--- recipes/security.rb
+++ recipes/security.rb
@@ -1,15 +1,16 @@
-hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:8181"
-if node.attribute? "hopsworks"
-  if node["hopsworks"].attribute? "https" and node["hopsworks"]['https'].attribute? ('port')
-    hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:#{node['hopsworks']['https']['port']}"
+if node["kagent"]["enabled"].casecmp?("true")
+  hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:8181"
+  if node.attribute? "hopsworks"
+    if node["hopsworks"].attribute? "https" and node["hopsworks"]['https'].attribute? ('port')
+      hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:#{node['hopsworks']['https']['port']}"
+    end
   end
-end
 
-crypto_dir = x509_helper.get_crypto_dir(node['consul']['user'])
-kagent_hopsify "Generate x.509" do
+  crypto_dir = x509_helper.get_crypto_dir(node['consul']['user'])
+  kagent_hopsify "Generate x.509" do
     user node['consul']['user']
     crypto_directory crypto_dir
     hopsworks_alt_url hopsworks_alt_url
     action :generate_x509
-    not_if { node["kagent"]["enabled"] == "false" }
+  end
 end
