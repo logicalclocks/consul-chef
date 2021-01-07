2e7d147a1d39fe57fb5c74fad8ba51effc0034e9
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2021-01-07 11:16:33 +0100

    [HOPSWORKS-2241] Get Hopsworks private IP only if needed (#26)
    * [HOPSWORKS-2241] Get Hopsworks private IP only if needed
    
    * [HOPSWORKS-2241] Create user x509 directory even when kagent is disabled
    
    * [HOPSWORKS-2241] Remove redundant argument

--- recipes/security.rb
+++ recipes/security.rb
@@ -1,15 +1,24 @@
-hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:8181"
-if node.attribute? "hopsworks"
-  if node["hopsworks"].attribute? "https" and node["hopsworks"]['https'].attribute? ('port')
-    hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:#{node['hopsworks']['https']['port']}"
+if node["kagent"]["enabled"].casecmp?("true")
+  hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:8181"
+  if node.attribute? "hopsworks"
+    if node["hopsworks"].attribute? "https" and node["hopsworks"]['https'].attribute? ('port')
+      hopsworks_alt_url = "https://#{private_recipe_ip("hopsworks","default")}:#{node['hopsworks']['https']['port']}"
+    end
   end
-end
 
-crypto_dir = x509_helper.get_crypto_dir(node['consul']['user'])
-kagent_hopsify "Generate x.509" do
+  crypto_dir = x509_helper.get_crypto_dir(node['consul']['user'])
+  kagent_hopsify "Generate x.509" do
     user node['consul']['user']
     crypto_directory crypto_dir
     hopsworks_alt_url hopsworks_alt_url
     action :generate_x509
-    not_if { node["kagent"]["enabled"] == "false" }
+  end
+else
+  # Create just the user directory without generating the certificates
+  # It is needed when joining managed NDB nodes in Hopsworks cluster
+  kagent_hopsify "Create user x.509 directory" do
+    user node['consul']['user']
+    crypto_directory x509_helper.get_crypto_dir(node['consul']['user'])
+    action :create_user_directory
+  end
 end
