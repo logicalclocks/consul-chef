757f58987275ccebbba2464b0bdceade12d11318
Jim Dowling <jim@logicalclocks.com>
2021-04-19 16:00:08 +0200

    add more config files

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -9,12 +9,18 @@ else
     masters = ["provider=#{node['consul']['retry_join']['provider'].strip} tag_key=#{node['consul']['retry_join']['tag_key'].strip} tag_value=#{node['consul']['retry_join']['tag_value'].strip}"]
 end
 
-crypto_dir = x509_helper.get_crypto_dir(node['consul']['user'])
-hops_ca = "#{crypto_dir}/#{x509_helper.get_hops_ca_bundle_name()}"
-certificate = "#{crypto_dir}/#{x509_helper.get_certificate_bundle_name(node['consul']['user'])}"
-key = "#{crypto_dir}/#{x509_helper.get_private_key_pkcs8_name(node['consul']['user'])}"
+insecure=
+  if node['consul']['security'].eql? "true"  
+    crypto_dir = x509_helper.get_crypto_dir(node['consul']['user'])
+    hops_ca = "#{crypto_dir}/#{x509_helper.get_hops_ca_bundle_name()}"
+    certificate = "#{crypto_dir}/#{x509_helper.get_certificate_bundle_name(node['consul']['user'])}"
+    key = "#{crypto_dir}/#{x509_helper.get_private_key_pkcs8_name(node['consul']['user'])}"
+  else
+    insecure="insecure-"
+  end
+
 template "#{node['consul']['conf_dir']}/consul.hcl" do
-    source "config/slave.hcl.erb"
+    source "config/#{insecure}slave.hcl.erb"
     owner node['consul']['user']
     group node['consul']['group']
     mode 0750
